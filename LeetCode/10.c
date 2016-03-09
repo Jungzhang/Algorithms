0a997f8c1724e5d9bd05813f9f25204c293503a0
Jungzhang <zhanggen.jung@gmail.com>
2016-03-09 17:47:14 +0800

    add

---
+++ LeetCode/10.c
@@ -0,0 +1,42 @@
+/*************************************************************************
+	> File Name: 10.c
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
+	> Created Time: 2016年04月07日 星期四 15时44分24秒
+	> Description: LeetCode第十题,正则的匹配之 . 和 *
+ ************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int isMatch(char *s, char *p)
+{
+    //递归出口判断
+    if (p[0] == '\0') {
+        return s[0] == '\0';
+    }
+
+    //分情况解决匹配问题,一种带*一种不带*
+    if (p[1] == '*') { //带*
+        while (s[0] != '\0' && (p[0] == '.' || s[0] == p[0])) { //如果匹配成功
+            if (isMatch(s, p + 2)) { //先把带*的匹配掠过,对后边的进行匹配
+                return 1;
+            }
+            ++s;    //把s向后移动一位,然后再次匹配*前的元素(因为*前的元素可能出现多次)
+        }
+        return isMatch(s, p + 2);   //继续匹配剩下的
+    } else {  //不带*
+        //如果匹配成功
+        if (s[0] != '\0' && (p[0] == '.' || s[0] == p[0])) {
+            return isMatch(s + 1, p + 1);   //递归下一个元素匹配
+        } else { //没有匹配成功
+            return 0;
+        }
+    }
+    
+}
+
+int main(int argc, char *argv[])
+{
+    
+    return EXIT_SUCCESS;
+}
