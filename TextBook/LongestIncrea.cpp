7bd0b03c3993a451faedbf61f2ba1c4129789e73
Jungzhang <zhanggen.jung@gmail.com>
2016-03-28 18:42:08 +0800

    add

--- TextBook/LongestIncrea.cpp
+++ TextBook/LongestIncrea.cpp
@@ -3,7 +3,9 @@
 	> Author: Jung
 	> Mail: jungzhang@xiyoulinux.org  or  zhanggen.jung@gmail.com
 	> Created Time: 2016年03月30日 星期三 09时04分46秒
-	> Description:
+	> Description:求数组中最长递增子序列的长度，
+    如序列[10, 9, 2, 5, 3, 7, 101, 18]，则最长递增子序列是[2, 3, 7, 101]，
+    所以程序返回4。要求O（nlogn）的时间复杂度。
  ************************************************************************/
 #include <iostream>
 #include <cstdlib>
@@ -11,6 +13,7 @@
 
 class Solution {
 public:
+    //二分法查找第一个大于tar的数字，返回其下标
     std::vector<int>::size_type BinarySearch(const std::vector<int> &nums, const int &tar)
     {
         int hig = nums.size() - 1;
@@ -31,6 +34,11 @@ public:
         return low;
     }
     
+    
+    //1、扫描原数组中的数，先判断是否大于结果数组中的最后一个（最大数），如果大于结果数组中的最大数则将这个数加入结果数组。否则，在结果数组中寻找第一个大于扫描数的数字下标，然后替换。
+    //2、重复上述步骤直到把原数组遍历完。
+    //3、查找的方法可以使用二分法查找，还需要将原数组遍历一遍。所以时间复杂度为O（nlogn）。
+    //4、本思路只能求解出长度，不能解出序列。
     int lengthOfLIS(std::vector<int>& nums) {
         std::vector<int> result;
         std::vector<int>::size_type tmp, index;
