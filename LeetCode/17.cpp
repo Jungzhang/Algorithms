ea182d025c808bdf56c8304b845df77eabfdb4c6
Jungzhang <zhanggen.jung@gmail.com>
2016-04-14 18:50:06 +0800

    add

--- LeetCode/17.cpp
+++ LeetCode/17.cpp
@@ -13,9 +13,10 @@ class Solution {
 public:
     std::vector<std::string> letterCombinations(std::string digits) {
         std::vector<std::string> result;
+        //将手机的每个按键映射到以数字键为下标的数组中
         std::vector<std::string> phoneMap = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         const int size = digits.size();
-        std::vector<std::string> tmp(size);
+        std::vector<std::string> tmp(size);   //将数字字符串替换成每个数字键上的字母
         
         if (size == 0) {
             return result;
@@ -36,11 +37,11 @@ private:
     void func(std::vector<std::string> &result, std::string &tar, std::vector<std::string> &tmp,const int &len, int pos)
     {
         if (pos == len) {
-            result.push_back(tar);
+            result.push_back(tar);  //如果到最后一层了则将其压入结果vector
             return;
         }
         for (int i = 0; i < tmp[pos].size(); ++i) {
-            tar[pos] = tmp[pos][i];
+            tar[pos] = tmp[pos][i]; 
             func(result, tar, tmp, len, pos + 1);
         }
     }
@@ -49,7 +50,7 @@ private:
 int main(int argc, char *argv[])
 {
     Solution s;
-    s.letterCombinations("23");
+    s.letterCombinations("12");
     
     return EXIT_SUCCESS;
 }
