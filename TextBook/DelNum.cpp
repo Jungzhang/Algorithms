b5a1ca9af595f310944ddcb0bf81664df25e2960
zhanggen <zhanggen.jung@gmail.com>
2016-05-22 18:35:02 +0800

    add

---
+++ TextBook/DelNum.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: DelNum.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org
+	> Created Time: 2016年05月22日 星期日 18时34分32秒
+	> Description: 删数问题
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+
+    return EXIT_SUCCESS;
+}
