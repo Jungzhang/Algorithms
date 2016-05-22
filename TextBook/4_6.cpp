f80cf3a3e6dad5d595defe7d3a0ebbfda155e0e9
zhanggen <zhanggen.jung@gmail.com>
2016-05-22 18:33:39 +0800

    add

---
+++ TextBook/4_6.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 4_6.cpp
+	> Author: Jung
+	> Mail: jungzhang@xiyoulinux.org
+	> Created Time: 2016年05月11日 星期三 09时06分09秒
+	> Description: 最优服务次序问题
+ ************************************************************************/
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+
+    return EXIT_SUCCESS;
+}
